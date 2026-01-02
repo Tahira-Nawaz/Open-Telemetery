aed2887d7b7e76325ee8dfe40b233c58d9bed77a
Tahira-Nawaz <tnawaz@puffersoft.com>
2026-01-02 18:53:54 +0500

    first commit

--- index.php
+++ index.php
@@ -1,2 +1,14 @@
 <?php
-echo "PHP OK";
+// Enable PHP errors
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Test PHP execution
+echo "PHP reached index.php<br>";
+
+// Test OTEL inclusion (optional)
+require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/otel.php';
+
+// Print your name as test
+echo "Hello, Tahira! This is a test<br>";
