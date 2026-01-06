3b8eb1671d7af79172ffbe340417765a4894793e
Tahira-Nawaz <tnawaz@puffersoft.com>
2026-01-06 18:10:02 +0500

    first commit

--- otel.php
+++ otel.php
@@ -2,27 +2,12 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-use OpenTelemetry\SDK\Trace\TracerProvider;
-use OpenTelemetry\SDK\Trace\SpanProcessor\SimpleSpanProcessor;
-use OpenTelemetry\SDK\Trace\SpanExporter\OtlpHttpSpanExporterFactory;
-use OpenTelemetry\SDK\Resource\ResourceInfo;
-use OpenTelemetry\SemConv\ResourceAttributes;
+use OpenTelemetry\SDK\Sdk;
 
-// Create exporter using SDK factory (AUTOLOADED)
-$exporter = (new OtlpHttpSpanExporterFactory())->create([
-    'endpoint' => 'http://127.0.0.1:4318/v1/traces',
-]);
+// Build SDK using environment variables
+$sdk = Sdk::builder()->build();
 
-$tracerProvider = new TracerProvider(
-    spanProcessors: [
-        new SimpleSpanProcessor($exporter),
-    ],
-    resource: ResourceInfo::create([
-        ResourceAttributes::SERVICE_NAME => 'azure-php-app',
-    ])
-);
-
-$tracer = $tracerProvider->getTracer('app-tracer');
+$tracer = $sdk->getTracerProvider()->getTracer('azure-php-app');
 
 // Test span
 $span = $tracer->spanBuilder('startup-span')->startSpan();
