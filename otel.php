4d20d7d6e1566347acfbc3a80eee3850b336f44f
Tahira-Nawaz <tnawaz@puffersoft.com>
2026-03-17 15:33:10 +0500

    first commit

--- otel.php
+++ otel.php
@@ -1,30 +1,24 @@
 <?php
 require 'vendor/autoload.php';
 
-use OpenTelemetry\SDK\Trace\TracerProvider;
-use OpenTelemetry\SDK\Trace\SpanProcessor\SimpleSpanProcessor;
-use OpenTelemetry\Exporter\Otlp\OtlpHttpExporter;
+use ApplicationInsights\Telemetry_Client;
 
+// Replace with your Instrumentation Key from Azure
+$ikey = '6129d3c2-30b9-47eb-97ca-59a04b4c9b46';
 
+$telemetry = new Telemetry_Client();
+$telemetry->getContext()->setInstrumentationKey($ikey);
 
-// Create exporter to send telemetry to Application Insights
-$exporter = new OtlpHttpExporter(
-    endpoint: 'https://westus2-2.in.applicationinsights.azure.com/v1/traces',
-    headers: [
-        'InstrumentationKey' => '6129d3c2-30b9-47eb-97ca-59a04b4c9b46'
-    ]
-);
+// Track the current page request
+$telemetry->trackRequest("HomepageRequest", "http://tahira-app-1.azurewebsites.net/", time(), 100, "200");
 
-$tracerProvider = new TracerProvider(
-    spanProcessor: new SimpleSpanProcessor($exporter)
-);
+// Optional: track a custom event
+$telemetry->trackEvent("TestEvent", ["User" => "Tahira"]);
 
-$tracer = $tracerProvider->getTracer('tahira-app-tracer');
+// Optional: track exception (if any)
+// try { ... } catch (\Exception $e) { $telemetry->trackException($e); }
 
-// Example trace
-$span = $tracer->startAndActivateSpan('HomepageRequest');
-// Simulate some work
-usleep(100000); // 0.1 seconds
-$span->end();
+// Flush all telemetry to Application Insights
+$telemetry->flush();
 
-echo "Telemetry sent to Azure Application Insights! app-test";
+echo "Telemetry sent to Azure Application Insights!";
