2edaa33c1dcb3fa7a563d66c0ecb108a8a354e98
Tahira-Nawaz <tnawaz@puffersoft.com>
2026-03-18 14:53:29 +0500

    first commit

--- otel.php
+++ otel.php
@@ -8,21 +8,22 @@ use OpenTelemetry\SDK\Common\Attribute\Attributes;
 use OpenTelemetry\Contrib\Otlp\SpanExporter;
 use OpenTelemetry\Contrib\Otlp\OtlpHttpTransportFactory;
 
-// Azure connection
+// Azure OTLP connection
 $connectionString = "InstrumentationKey=688d2b7d-daea-49b0-9d30-b7d7ba5b02e1;IngestionEndpoint=https://westus2-2.in.applicationinsights.azure.com/;LiveEndpoint=https://westus2.livediagnostics.monitor.azure.com/;ApplicationId=705a3523-c215-4677-95cc-f86d159e2140";
 $endpoint = "https://westus2-2.otlp.applicationinsights.azure.com/v1/traces";
 
-// Create SpanExporter
-$exporter = new SpanExporter(
-    transportFactory: new OtlpHttpTransportFactory(
-        endpoint: $endpoint,
-        headers: [
-            "Authorization" => "Bearer $connectionString"
-        ]
-    )
+// Create transport (positional args)
+$transportFactory = new OtlpHttpTransportFactory(
+    $endpoint, // endpoint
+    [
+        "Authorization" => "Bearer $connectionString"
+    ]
 );
 
-// Resource info (your app name)
+// Create SpanExporter
+$exporter = new SpanExporter($transportFactory);
+
+// Add resource info (your app name)
 $resource = ResourceInfo::create(Attributes::create([
     "service.name" => "tahira-app-1"
 ]));
@@ -35,7 +36,7 @@ $tracerProvider = new TracerProvider(
 
 $tracer = $tracerProvider->getTracer("tahira-tracer");
 
-// Example span
+// Create span
 $span = $tracer->spanBuilder("HomepageRequest")->startSpan();
 usleep(100000); // simulate work
 $span->end();
