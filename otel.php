ee69ee648436dcabec121a591c076ff8afde2595
Tahira-Nawaz <tnawaz@puffersoft.com>
2026-03-18 13:40:13 +0500

    first commit

--- otel.php
+++ otel.php
@@ -3,26 +3,44 @@ require __DIR__ . '/vendor/autoload.php';
 
 use OpenTelemetry\SDK\Trace\TracerProvider;
 use OpenTelemetry\SDK\Trace\SpanProcessor\SimpleSpanProcessor;
-use OpenTelemetry\Contrib\OtlpHttp\Exporter as OtlpHttpExporter;
+use OpenTelemetry\Contrib\Otlp\OtlpHttpExporter;
+use OpenTelemetry\SDK\Resource\ResourceInfo;
+use OpenTelemetry\SDK\Common\Attribute\Attributes;
 
+// 👉 Use your FULL connection string here
+$connectionString = "InstrumentationKey=688d2b7d-daea-49b0-9d30-b7d7ba5b02e1;IngestionEndpoint=https://westus2-2.in.applicationinsights.azure.com/;LiveEndpoint=https://westus2.livediagnostics.monitor.azure.com/;ApplicationId=705a3523-c215-4677-95cc-f86d159e2140";
 
-// OTLP Exporter for Application Insights
+// Azure OTLP endpoint
+$endpoint = "https://westus2-2.otlp.applicationinsights.azure.com/v1/traces";
+
+// Correct exporter
 $exporter = new OtlpHttpExporter(
-    endpoint: 'https://westus2-2.otlp.applicationinsights.azure.com/v1/traces',
+    endpoint: $endpoint,
     headers: [
-        'Authorization' => 'InstrumentationKey 6129d3c2-30b9-47eb-97ca-59a04b4c9b46'
+        "Authorization" => "Bearer $connectionString"
     ]
 );
 
+// Add service name (important for logs visibility)
+$resource = ResourceInfo::create(Attributes::create([
+    "service.name" => "tahira-php-app"
+]));
+
 $tracerProvider = new TracerProvider(
-    spanProcessor: new SimpleSpanProcessor($exporter)
+    new SimpleSpanProcessor($exporter),
+    $resource
 );
 
-$tracer = $tracerProvider->getTracer('tahira-app-tracer');
+$tracer = $tracerProvider->getTracer("tahira-tracer");
 
-// Example trace
-$span = $tracer->startAndActivateSpan('HomepageRequest');
-usleep(100000); // simulate work
+// Start span
+$span = $tracer->spanBuilder("HomepageRequest")->startSpan();
+
+usleep(100000);
+
 $span->end();
+
+// IMPORTANT: send data
+$tracerProvider->shutdown();
 
 echo "Telemetry sent to Azure Application Insights!";
